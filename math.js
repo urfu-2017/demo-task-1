36aa672a1dfa9cf4e1c58fcb084f383f42893a13
Ftlka <smart.Julia@yandex.ru>
2017-09-28 21:32:33 +0500

    third commit

--- math.js
+++ math.js
@@ -4,10 +4,11 @@ exports.isStar = true;
 
 exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
-    let cUndefined = 0;
-    if (c != undefined)
-    {
+    let cUndefined;
+    if (c === undefined)
+        cUndefined = 0;
+    else 
         cUndefined = Number(c);
-    }
+    
     return Number(a) + Number(b) + cUndefined;
 };
