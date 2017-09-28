009d89444599a9abab02386eb85a1e95b83fd8c7
Hazipov Vladislav <hazipov.vlad@yandex.ru>
2017-09-29 00:14:58 +0500

    hope never dies

--- math.js
+++ math.js
@@ -3,7 +3,9 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-if(isNaN(arguments[2])) return Number(a) + Number(b);	
+if(isNaN(arguments[2])){ 
+	return Number(a) + Number(b);
+}
 return Number(a) + Number(b) + Number(arguments[2]);
 };
 
