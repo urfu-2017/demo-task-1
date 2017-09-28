31f5cc2e93852856d84a7c2020f57ef9b65ade2a
fng3r <dinvk@mail.ru>
2017-09-28 21:50:58 +0500

    fix bug

--- math.js
+++ math.js
@@ -2,8 +2,8 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
-    let c = arguments[0] || 0;
+exports.sum = function (a, b, c) {
+    c = c || 0;
 
     return Number(a) + Number(b) + Number(c);
 };
