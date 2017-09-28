b9b4225e0ea7681fb4687fee24f0a71f94702606
Ftlka <smart.Julia@yandex.ru>
2017-09-28 21:56:58 +0500

    seems like i deleted all the trailing spaces

--- math.js
+++ math.js
@@ -5,8 +5,9 @@ exports.isStar = true;
 exports.sum = function (a, b, c) {
     // Реализуйте сложение в этой функции
     let cUndefined = 0;
-    if (c !== undefined){ 
+    if (c !== undefined) {
         cUndefined = Number(c);
     }
+
     return Number(a) + Number(b) + cUndefined;
 };
