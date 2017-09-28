e74535983d21150ca47ff2859f6702442c3ac2e2
HazipovVF <hazipov.vlad@yandex.ru>
2017-09-29 01:02:13 +0500

    Update math.js

--- math.js
+++ math.js
@@ -3,11 +3,11 @@
 exports.isStar = true;
 
 exports.sum = function (a, b) {
-    if(isNaN(arguments[2])){
+    if (isNaN(arguments[2])) {
         return Number(a) + Number(b);
-    }else{
-        return Number(a) + Number(b) + Number(arguments[2]);
-    }
+    } 
+    return Number(a) + Number(b) + Number(arguments[2]);
+    
 };
 
 
