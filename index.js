7f1d577e1afc84e96b2dd77ec6435201dbf0bf8a
Nikolai Borodin <meta1-heart@yandex.ru>
2017-09-30 00:41:26 +0500

    realization of summ of two numbers

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 
 const { sum } = require('./math');
 
-console.info(sum(1, 2));
-// 3
+console.info(sum(1, '2', 3));
+// 6
 
 console.info(sum(63, 15));
 // 78
 
 console.info(sum('12', 7));
 // 19
+
+console.info(sum(3, 3, 10));
+//16
