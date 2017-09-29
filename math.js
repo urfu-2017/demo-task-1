6f96cf75f323e524d8cbd537a3a0d9758eca4254
KrazyD <Svalovdmitrii@mail.ru>
2017-09-29 21:38:22 +0500

    Вторая попытка

--- math.js
+++ math.js
@@ -4,9 +4,8 @@ exports.isStar = true;
 
 exports.sum = function (a, b, c) {
     if (arguments.length === 3) {
-        
         return parseInt(a) + parseInt(b) + parseInt(c);
     }
-    
+
     return parseInt(a) + parseInt(b);
 };
