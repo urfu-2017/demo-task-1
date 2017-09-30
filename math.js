e7ae25494681e0ea09fed781eafb1009dfa8ef51
rkrk117 <32203360+rkrk117@users.noreply.github.com>
2017-10-01 00:23:52 +0500

    Add files via upload

--- math.js
+++ math.js
@@ -3,7 +3,9 @@
 exports.isStar = true;
 
 exports.sum = function (a, b, c) {
-    if (c === undefined)
+    if (c === undefined) {
         return Number(a) + Number(b);
+    }
+
     return Number(a) + Number(b) + Number(c);
 };
