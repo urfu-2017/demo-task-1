58693faf14ce8ff3be75d4eaf934c0848cde81ec
chrighter <christina.ilyenko@yandex.ru>
2017-10-01 21:46:02 +0500

    sum

--- index.js
+++ index.js
@@ -2,11 +2,20 @@
 
 const { sum } = require('./math');
 
-console.info(sum(1, 2));
+console.info(sum('1', 2));
 // 3
+console.info(sum(1, 2));
 
-console.info(sum(63, 15));
+console.info(sum('кек', 'кек', 'undefined'));
 // 78
 
-console.info(sum('12', 7));
+console.info(sum('12.fv53b', '7', '19'));
 // 19
+
+console.info(sum('a', '21', '1'));
+
+console.info(sum('12.3', 7, '19'));
+
+console.info(sum(12, 7, 19));
+
+console.info(sum(undefined, undefined, undefined));
