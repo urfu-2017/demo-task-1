cbc6bbedec78d49f0c148cfd7f0856e549048b72
impuppy <31798367+impuppy@users.noreply.github.com>
2017-10-01 20:09:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 const { sum } = require('./math');
 
-console.info(sum(1, 2));
-// 3
+console.info(sum(1, 2, 3));
+// 6
 
-console.info(sum(63, 15));
-// 78
+console.info(sum(63, 15, 29));
+// 107
 
-console.info(sum('12', 7));
-// 19
+console.info(sum('12', 7, 5));
+// 24
