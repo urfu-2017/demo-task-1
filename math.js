28190d78b4294f728965781fbd9082e1a9aa65ac
impuppy <31798367+impuppy@users.noreply.github.com>
2017-10-01 19:37:23 +0500

    Update math.js

--- math.js
+++ math.js
@@ -1,7 +1,7 @@
 'use strict';
 
-exports.isStar = true;
+exports.isStar = false;
 
-exports.sum = function (a, b, c) {
-    return Number(a) + Number(b) + Number(c); 
+exports.sum = function (a, b) {
+    return Number(a) + Number(b); 
 };
