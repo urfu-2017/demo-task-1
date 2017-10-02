aab03e029b8e709ade1830dbc26fca16d29e7ea6
Kuzmin Dmitry <dmitry@rung.pro>
2017-10-02 18:02:49 +0500

    i dont know 2

--- math.js
+++ math.js
@@ -5,6 +5,6 @@ exports.isStar = true;
 exports.sum = function (a, b, c) {
     if (c !== undefined) {
         return Number(a) + Number(b) + Number(c);
-    }
+    };
     return Number(a) + Number(b);
 };
