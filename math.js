0ea2e854f61925e12d7944edc5dfdc2bbff5c02d
Kuzmin Dmitry <dmitry@rung.pro>
2017-10-02 18:15:41 +0500

    finally fix linting

--- math.js
+++ math.js
@@ -3,9 +3,10 @@
 exports.isStar = true;
 
 exports.sum = function () {
-    var result=0;
+    var result = 0;
     for (var i = 0; i < arguments.length; i++) {
         result += Number(arguments[i]);
-      }
+    }
+
     return result;
 };
