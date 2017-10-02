fdde83584155bce9b5ea26c4301f1e13541e1603
WBDiamond <360mlgnoscope420blazeit@gmail.com>
2017-10-02 23:21:04 +0500

    решение добавлено

--- math.js
+++ math.js
@@ -1,5 +1,5 @@
 'use strict';
-exports.isStar = false;
-exports.sum = function (a, b) {
-    return Number(a) + Number(b);
+exports.isStar = true;
+exports.sum = function (a, b, c) {
+    return Number(a) + Number(b) + Number(c);
 };
