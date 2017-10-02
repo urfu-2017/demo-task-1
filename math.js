b2161a69ea75f0d66a8d392bab8af604388f9349
Kuzmin Dmitry <dmitry@rung.pro>
2017-10-02 17:43:01 +0500

    add many args and Number check

--- math.js
+++ math.js
@@ -5,7 +5,7 @@ exports.isStar = true;
 exports.sum = function () {
     var result=0;
     for (var i = 0; i < arguments.length; i++) {
-        result += arguments[i];
+        result += Number(arguments[i]);
       }
     return result;
 };
