5940e3172d325e924f1f0a2a304c6dd5a9d9aa62
Kuzmin Dmitry <dmitry@rung.pro>
2017-10-02 18:10:26 +0500

    lets begin

--- math.js
+++ math.js
@@ -2,9 +2,10 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b, c) {
-    if (c !== undefined) {
-        return Number(a) + Number(b) + Number(c);
-    };
-    return Number(a) + Number(b);
+exports.sum = function () {
+    var result=0;
+    for (var i = 0; i < arguments.length; i++) {
+        result += Number(arguments[i]);
+      }
+    return result;
 };
