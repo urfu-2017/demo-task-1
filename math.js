166563823666ca060fee51a3360b009aea8eefaf
spt30 <31798669+spt30@users.noreply.github.com>
2017-10-04 02:33:56 +0500

    qwer

--- math.js
+++ math.js
@@ -4,10 +4,11 @@ exports.isStar = true;
 
 exports.sum = function (a, b, c) {
     let NumC = Number(c);
+    var result = 0;
     if (Number.isNaN(NumC)) {
-        let result = Number(a) + Number(b);
+        result = Number(a) + Number(b);
     } else {
-        let result = Number(a) + Number(b) + NumC;
+        result = Number(a) + Number(b) + NumC;
     }
     return result;
 };
