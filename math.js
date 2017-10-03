a2cdf5aeebc34411c18e56b27a3be23d6b3f18d0
Alexey <a.dubrovin@skbkontur.ru>
2017-10-03 18:15:23 +0500

    add * solve

--- math.js
+++ math.js
@@ -2,6 +2,11 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
-    return parseInt(a) + parseInt(b);
+exports.sum = function (a, b, c) {
+    let res = parseInt(a) + parseInt(b);
+    if (c !== undefined) {
+        res += parseInt(c);
+    }
+
+    return res;
 };
