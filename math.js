5b46cca9c0b2308b1139f623b7fc7f646ee2e1ca
spt30 <31798669+spt30@users.noreply.github.com>
2017-10-04 02:00:33 +0500

    2 вариант (еще раз исправленный)

--- math.js
+++ math.js
@@ -3,5 +3,10 @@
 exports.isStar = true;
 
 exports.sum = function (a, b, c){
-    return (a + b + c);
+    let NumC = Number(c);
+    if  (Number.isNaN(c)){
+        return ( Number(a) + Number(b) );
+    } else {
+        return ( Number(a) + Number(b) + NumC);
+    }
 }
