6ee53dbeee2feaf131cd79fc35f985b672102537
AZ404 <ztbzak1@rambler.ru>
2017-10-03 18:17:35 +0500

    added cast to Number

--- math.js
+++ math.js
@@ -3,9 +3,9 @@
 exports.isStar = true;
 
 exports.sum = function (a, b, c) {
-    var result = a + b;
+    var result = Number(a) + Number(b);
     if (typeof c !== 'undefined') {
-        result += c;
+        result += Number(c);
     }
 
     return result;
