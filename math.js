d82ba1c183ba028674625aec425911e071ae9341
Naturin <bontortue@gmail.com>
2017-10-07 22:22:14 +0500

    v.1

--- math.js
+++ math.js
@@ -2,6 +2,12 @@
 
 exports.isStar = true;
 
-exports.sum = function (a, b) {
-    // Реализуйте сложение в этой функции
+exports.sum = function (a, b, c) {
+    if (c === undefined) {
+        c = 0;
+    }
+
+    return Number(a) + Number(b) + Number(c);
+
 };
+
